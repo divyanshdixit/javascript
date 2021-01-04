26a79c0e62ba76ce75920c3d1cd8847b0b3a8873
divyansh dixit <divyanshdixit96@gmail.com>
2021-01-04 12:58:43 +0530

    js objects

---
+++ js/index.js
@@ -0,0 +1,69 @@
+for(i=0; i<10; i++){ 
+
+}
+
+arr = [1, 2, 3, 4, 5]; // 
+var c = 4;
+4, index = 3
+-1
+
+
+//  +, - . * , / , %
+
+// ** Exponential 
+
+// && logical and  || 
+
+// & bitwise and 
+
+// true & true = true 
+// false || false = false
+
+var x = 1;
+var y = 2;
+
+// x = x + 5;
+x += 5;
+console.log(x);
+
+// conditonal 
+
+var age = 19;
+
+
+if(age > 18){
+       welcome
+}else if( age > 18){
+welcome 
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else if(age < 18){
+
+}else{
+
+}
+
+switch 
+        case : (age < 18)
+                asdas
+                break continue default
+        case : age > 18
+
+
+
