4e138f73a9cd309b4c5ddf4200851a2599fccb68
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-02-14 16:59:08 +0530

    dom, error,pattern

--- js/pattern.js
+++ js/pattern.js
@@ -57,7 +57,9 @@ var i, j;
 for(i=1; i<=5;i++){
    
     for(j=1; j<=count; j++){
-        space = space + ' ';
+        // space = space + ' ';
+        space= ` ${space}`;
+        document.write(space);
     }
 
     space = space + star; // &&&&*
